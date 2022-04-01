366b9d958c5282fd3686bb42b660f1327b8560af
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-04-01 13:54:09 +0200

    Created overview attribute to MovieDetail, so that an overview of a movie can be retrieved from the API.

--- app/src/main/java/com/example/bioscoopapp/Domain/MovieDetail.java
+++ app/src/main/java/com/example/bioscoopapp/Domain/MovieDetail.java
@@ -36,6 +36,9 @@ public class MovieDetail implements Parcelable {
     @SerializedName("original_title")
     @Expose
     private String originalTitle;
+    @SerializedName("overview")
+    @Expose
+    private String overview;
     @SerializedName("popularity")
     @Expose
     private Double popularity;
@@ -201,6 +204,14 @@ public class MovieDetail implements Parcelable {
 
     public void setOriginalTitle(String originalTitle) {
         this.originalTitle = originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
     }
 
     public Double getPopularity() {
